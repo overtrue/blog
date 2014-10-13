31a3fbb743a2ca6b9a4bbdf0eaba179585370973
overtrue <anzhengchao@gmail.com>
2014-10-13 10:24:51 +0800

    更新css

--- assets/js/sns.js
+++ assets/js/sns.js
@@ -1,11 +1,22 @@
-var pageTitle = document.title;
-var pageUrl   = encodeURIComponent(window.location.href);
-var siteTitle = $(document.head).find('[name="site-title"]').text();
-var pageDesc  = $(document.head).find('[name="description"]').text();
+/**
+ * SNS 分享组件
+ *
+ * @author Carlos <anzhengchao@gmail.com>
+ * @url    http://overtrue.me
+ * @github https://github.com/overtrue/SNS
+ */
+;
+if("undefined" == typeof $) {
+	console.log('no jQuery found!');
+};
+var pageTitle  = document.title;
+var pageUrl    = encodeURIComponent(window.location.href);
+var siteTitle  = $(document.head).find('[name="site-title"]').text();
+var pageDesc   = $(document.head).find('[name="description"]').text();
 var firstImage = $(document).find('img:first');
-var pageImage = (firstImage) ? firstImage.attr('src') : '';
+var pageImage  = (firstImage) ? firstImage.attr('src') : '';
 
-var  SNS ={
+var SNS ={
 	config:{
 		url    : '',
 		site   : '',//来源
@@ -17,11 +28,11 @@ var  SNS ={
 
 	share:function(type){
 		var share_url,
-			url   = this.config.url || pageUrl,
+			url   = this.config.url   || pageUrl,
 			title = this.config.title || pageTitle,
-			pic   = this.config.pic || pageImage,
-			desc  = this.config.desc || pageDesc;
-			site  = this.config.site || siteTitle;
+			pic   = this.config.pic   || pageImage,
+			desc  = this.config.desc  || pageDesc;
+			site  = this.config.site  || siteTitle;
 
 		switch(type){
 			case 'sina':
@@ -41,20 +52,23 @@ var  SNS ={
 				break;
 		}
 
-		this.openNew(share_url);
+		this.open_window(share_url);
 	},
-	getParamsOfShareWindow:function(width,height){
+
+	get_window_params: function(width,height) {
 		var top = (document.body.clientHeight-height) / 2;
 		var left = (document.body.clientWidth-width) / 2;
 		return ['toolbar=0,status=0,resizable=1,width=' + width + ',height=' + height + ',left='+left+',top='+top].join('');
 	},
-	openNew:function(url){
-		window.open(url,this.w, this.getParamsOfShareWindow(680,420));
+
+	open_window: function(url) {
+		window.open(url,this.w, this.get_window_params(680,420));
 	},
-	init:function(newConfig){
-		for(var i in newConfig){
-			if(newConfig[i]){
-				this.config[i]=newConfig[i];
+
+	init: function(custom_config) {
+		for(var i in custom_config){
+			if(custom_config[i]){
+				this.config[i]=custom_config[i];
 			}
 		}
 	}
