dc6a70016dad6c790f726ce7a25fb65d6c7ce053
overtrue <anzhengchao@gmail.com>
2014-10-13 09:32:37 +0800

    添加QQ空间分享

--- assets/js/sns.js
+++ assets/js/sns.js
@@ -1,5 +1,6 @@
 var pageTitle = document.title;
 var pageUrl   = encodeURIComponent(window.location.href);
+var siteTitle = $(document.head).find('[name="site-title"]').text();
 var pageDesc  = $(document.head).find('[name="description"]').text();
 var firstImage = $(document).find('img:first');
 var pageImage = (firstImage) ? firstImage.attr('src') : '';
@@ -7,6 +8,7 @@ var pageImage = (firstImage) ? firstImage.attr('src') : '';
 var  SNS ={
 	config:{
 		url    : '',
+		site   : '',//来源
 		title  : '',
 		desc   : '',
 		pic    : '',
@@ -19,18 +21,21 @@ var  SNS ={
 			title = this.config.title || pageTitle,
 			pic   = this.config.pic || pageImage,
 			desc  = this.config.desc || pageDesc;
+			site  = this.config.site || siteTitle;
 
 		switch(type){
-			case 1:
+			case 'sina':
 				share_url="http://service.weibo.com/share/share.php?url=" + url + "&title=" + title + desc + (pic ? "&pic=" + pic : '');
 			break;
-			case 2:
+			case 'qq':
 				share_url="http://share.v.t.qq.com/index.php?c=share&a=index&url=" + url + "&title=" + title + desc + (pic ? "&pic=" + pic : '');
 			break;
-			case 3:
+			case 'qzone':
+				share_url="http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=" + url + "&title=" + title + "&desc=" + desc + "&summary=" + desc + "&site=" + site + (pic? pic : '');
+			case 'renren':
 				share_url="http://widget.renren.com/dialog/share?resourceUrl=" + url + "&title="+ title +"&description=" + desc + url + (pic ? "&pic=" + pic : '');
 			break;
-			case 5:
+			case 'douban':
 				share_url="http://shuo.douban.com/!service/share?href=" + url + "&name=" + title + "&text=" + desc + ( pic ? "&image=" + pic : '') + '&starid=0&aid=0&style=11&stime=&sig=';
 			break;
 		}
@@ -38,8 +43,8 @@ var  SNS ={
 		this.openNew(share_url);
 	},
 	getParamsOfShareWindow:function(width,height){
-		var top=(document.body.clientHeight-height)/2;
-		var left=(document.body.clientWidth-width)/2;
+		var top = (document.body.clientHeight-height) / 2;
+		var left = (document.body.clientWidth-width) / 2;
 		return ['toolbar=0,status=0,resizable=1,width=' + width + ',height=' + height + ',left='+left+',top='+top].join('');
 	},
 	openNew:function(url){
