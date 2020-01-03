78c8fac295bcd94fc479d1df16db5c2528d30caa
overtrue <anzhengchao@gmail.com>
2020-01-03 10:48:57 +0800

    更新构建脚本

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-cd $DIR && git reset --hard && git pull && bundle install && npm i && grunt build
+cd $DIR && git reset --hard && git pull && bundle install && npm i && grunt build && \
+
+curl 'https://api.github.com/users/overtrue/repos?type=owner&per_page=100' -H 'Accept: */*' -H 'Referer: https://overtrue.me/open-source/' -H 'Origin: https://overtrue.me' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36' --compressed > /www/overtrue.me/_site/opensource.json
